e6a903baabb3d4dc035cfdd1d2c6092b131916c0
cuixiaoyiyi <cbq@pku.edu.cn>
2022-05-06 21:20:07 +0800

    Update AndroidLayoutControl.java

--- project/Thread-Leopard/src/jymbolic/android/resources/controls/AndroidLayoutControl.java
+++ project/Thread-Leopard/src/jymbolic/android/resources/controls/AndroidLayoutControl.java
@@ -1,13 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2012 Secure Software Engineering Group at EC SPRIDE.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Lesser Public License v2.1
- * which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- * 
- * Contributors: Christian Fritz, Steven Arzt, Siegfried Rasthofer, Eric
- * Bodden, and others.
- ******************************************************************************/
 package jymbolic.android.resources.controls;
 
 import java.util.HashMap;
@@ -17,12 +7,7 @@ import jymbolic.android.resources.xml.AXmlAttribute;
 import pxb.android.axml.NodeVisitor;
 import soot.SootClass;
 
-/**
- * Data class representing a layout control on the android screen
- * 
- * @author Steven Arzt
- *
- */
+
 public class AndroidLayoutControl {
 
 	protected int id;
