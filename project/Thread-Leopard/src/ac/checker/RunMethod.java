8b1618832b949e1011a378a2fb2254aa71b9cc56
cuixiaoyiyi <cbq@pku.edu.cn>
2022-05-06 22:03:27 +0800

    Update RunMethod.java

--- project/Thread-Leopard/src/ac/checker/RunMethod.java
+++ project/Thread-Leopard/src/ac/checker/RunMethod.java
@@ -1,21 +1,3 @@
-/* AsyncDetecotr - an Android async component misuse detection tool
- * Copyright (C) 2018 Baoquan Cui
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
- */
 package ac.checker;
 
 import java.util.ArrayList;
@@ -33,12 +15,6 @@ import soot.jimple.Stmt;
 import soot.toolkits.graph.BriefUnitGraph;
 import soot.toolkits.graph.UnitGraph;
 
-/**
- * The loop summary of method
- * 
- * @author Baoquan Cui
- * @version 1.0
- */
 public class RunMethod {
 
 	private static final Map<String, RunMethod> doInBackgroundMethods = new HashMap<>();
