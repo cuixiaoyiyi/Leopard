f0b6c638cac4b557c284a13c041b7baafc06ccb8
cuixiaoyiyi <cbq@pku.edu.cn>
2022-05-06 21:23:51 +0800

    Update IResourceHandler.java

--- project/Thread-Leopard/src/jymbolic/android/resources/xml/parser/IResourceHandler.java
+++ project/Thread-Leopard/src/jymbolic/android/resources/xml/parser/IResourceHandler.java
@@ -1,24 +1,8 @@
-/*******************************************************************************
- * Copyright (c) 2012 Secure Software Engineering Group at EC SPRIDE.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Lesser Public License v2.1
- * which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- * 
- * Contributors: Christian Fritz, Steven Arzt, Siegfried Rasthofer, Eric
- * Bodden, and others.
- ******************************************************************************/
 package jymbolic.android.resources.xml.parser;
 
 import java.io.InputStream;
 import java.util.Set;
 
-/**
- * Common interface for handlers working on Android resource XML files
- * 
- * @author Steven Arzt
- *
- */
 public interface IResourceHandler {
 	
 	/**
