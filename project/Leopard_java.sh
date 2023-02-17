b537257e7949fd8e79884efc9f4dade27e2206b1
cuixiaoyiyi <cbq@pku.edu.cn>
2023-02-17 18:47:24 +0800

    update tools

--- project/Leopard_java.sh
+++
@@ -1,9 +0,0 @@
-java -cp sumThread.jar ac.LeopardMain  SpringFramework5.3.18 >SpringLog.log 2>&1
-java -cp sumThread.jar ac.LeopardMain  apache-tomcat-10.0.20 >tomcatLog.log 2>&1
-java -cp sumThread.jar ac.LeopardMain  quarkus-cli-2.8.0.Final >quarkusLog.log 2>&1
-java -cp sumThread.jar ac.LeopardMain  wildfly-26.1.0.Beta1 >wildflyLog.log 2>&1
-java -cp sumThread.jar ac.LeopardMain  hivemq-ce-2021.3 >hivemq-ceLog.log 2>&1
-java -cp sumThread.jar ac.LeopardMain  jetty-distribution-9.4.46.v20220331 >jetty-distributionLog.log 2>&1
-java -cp sumThread.jar ac.LeopardMain  micronaut-cli-3.4.2 >micronautLog.log 2>&1
-java -cp sumThread.jar ac.LeopardMain  apache-cxf-3.5.2 >cxfLog.log 2>&1
-java -cp sumThread.jar ac.LeopardMain  Junit5 >Junit5_.log 2>&1
